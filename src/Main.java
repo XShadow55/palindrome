3c30f08d3c9ff55bb854915946aa10d2effb124d
Свинухов константин <lopata216@yandex.ru>
2023-09-15 19:11:37 +0300

    задача из билета

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,19 @@ import java.util.Random;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-
+    public static void main(String[] arg) {
+        String palindrom = "шалаш";
+        char[] masiv = palindrom.toCharArray();
+        char[] masiv1 = new char[masiv.length];
+        for (int i = masiv.length - 1; i > -1; i--) {
+            masiv1[masiv.length - i - 1] = masiv[i];
+        }
+        String palindrom1 = new String(masiv1);
+        if (palindrom.equals(palindrom1)){
+            System.out.println("Палиндром");
+        }
+        else {
+            System.out.println("Обычное слово");
+        }
+    }
 }
